5cd1252efdb2ecb54bab0af50c7a3b8d98cd5fec
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-06-22 05:10:55 +0100

    Adopt `target_sources_grouped` from OpenXRay `cmake/utils`

--- cmake/util.cmake
+++ cmake/util.cmake
@@ -1,3 +1,32 @@
+# Given a target, name, scope, and list of files,
+# add the files as sources and group them with the given name
+# From OpenXRay `cmake/utils.cmake`
+function(target_sources_grouped)
+    cmake_parse_arguments(
+        PARSED_ARGS
+        ""
+        "TARGET;NAME;SCOPE"
+        "FILES"
+        ${ARGN}
+    )
+
+    if(NOT PARSED_ARGS_TARGET)
+        message(FATAL_ERROR "You must provide a target name")
+    endif()
+
+    if(NOT PARSED_ARGS_NAME)
+        message(FATAL_ERROR "You must provide a source group name")
+    endif()
+
+    if(NOT PARSED_ARGS_SCOPE)
+        set(PARSED_ARGS_SCOPE PRIVATE)
+    endif()
+
+    target_sources(${PARSED_ARGS_TARGET} ${PARSED_ARGS_SCOPE} ${PARSED_ARGS_FILES})
+
+    source_group(${PARSED_ARGS_NAME} FILES ${PARSED_ARGS_FILES})
+endfunction()
+
 # Disable CXX exceptions for the current scope
 macro(disable_exceptions)
   string(REPLACE "/EHsc" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
