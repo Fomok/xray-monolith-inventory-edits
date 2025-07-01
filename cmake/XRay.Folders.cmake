0638e5056c7c2a274af20e9f9cab2da15bd228d0
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-01 22:19:11 +0100

    Move `SDK` to `External/SDK`

--- cmake/XRay.Folders.cmake
+++ cmake/XRay.Folders.cmake
@@ -7,10 +7,10 @@ set_property(GLOBAL PROPERTY PREDEFINED_TARGETS_FOLDER CMake)
 set_property(GLOBAL PROPERTY USE_FOLDERS ON)
 
 # Define folder names
-set(FOLDER_SDK SDK)
 set(FOLDER_EXTERNAL External)
 set(FOLDER_LUA External/Lua)
 set(FOLDER_LUA_BUILD External/Lua/Build)
+set(FOLDER_SDK External/SDK)
 set(FOLDER_XRAY X-Ray)
 set(FOLDER_XRAY_CDB X-Ray/CDB)
 set(FOLDER_XRAY_GAME X-Ray/Game)
