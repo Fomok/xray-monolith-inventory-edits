d0a0329fac421646171632ea595ff8471488ec45
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-01 21:33:42 +0100

    Split AI, Physics out of xrGame, create dedicated subproject files

---
+++ src/xrGame/cmake/XRay.Game.Physics.cmake
@@ -0,0 +1,84 @@
+add_library(xrGame.Physics STATIC)
+
+set_property(
+  TARGET xrGame.Physics
+  PROPERTY FOLDER
+  ${FOLDER_XRAY_GAME}
+)
+
+target_sources(xrGame.Physics PRIVATE
+  ../xrServerEntities/PHNetState.cpp
+  PHCollisionDamageReceiver.cpp
+  PHCommander.cpp
+  PHDebug.cpp
+  PHDestroyable.cpp
+  PHDestroyableNotificate.cpp
+  PHMovementControl.cpp
+  PHMovementDynamicActivate.cpp
+  PHScriptCall.cpp
+  PHShellCreator.cpp
+  PHSimpleCalls.cpp
+  PHSimpleCallsScript.cpp
+  PHSkeleton.cpp
+  PHSoundPlayer.cpp
+  PhysicObject.cpp
+  PhysicObject_script.cpp
+  PhysicsGamePars.cpp
+  PhysicsShellHolder.cpp
+  PhysicsSkeletonObject.cpp
+  physics_element_scripted.cpp
+  physics_game.cpp
+  physics_joint_scripted.cpp
+  physics_shell_animated.cpp
+  physics_shell_scripted.cpp
+  physics_world_scripted.cpp
+  physic_item.cpp
+  
+  ../xrServerEntities/PHNetState.h
+  ../xrServerEntities/PHSynchronize.h
+  PHCollisionDamageReceiver.h
+  PHCommander.h
+  PHDebug.h
+  PHDestroyable.h
+  PHDestroyableNotificate.h
+  PHMovementControl.h
+  PHReqComparer.h
+  PHScriptCall.h
+  PHShellCreator.h
+  PHSimpleCalls.h
+  PHSkeleton.h
+  PHSoundPlayer.h
+)
+
+target_include_directories(xrGame.Physics
+  PUBLIC
+  ${CMAKE_CURRENT_SOURCE_DIR}
+  PRIVATE
+  ${CMAKE_SOURCE_DIR}/src/xrServerEntities
+)
+
+target_compile_definitions(xrGame.Physics
+  PRIVATE
+  XRGAME_EXPORTS
+)
+
+target_precompile_headers(xrGame.Physics
+  PRIVATE
+  $<$<COMPILE_LANGUAGE:CXX>:stdafx.h>
+)
+
+target_link_libraries(xrGame.Physics
+  PRIVATE
+  DPlay
+  imgui
+  luabind
+  LuaJIT
+  xrAPI
+  xrCDB
+  xrCore
+  xrEngine
+  xrRender
+  xrPhysics
+  xrSound
+  xrNetServer
+)
