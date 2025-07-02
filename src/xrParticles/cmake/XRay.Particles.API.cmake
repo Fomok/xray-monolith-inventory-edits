60118d3fd7703f943dd767703344d72b42a99f4d
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-02 02:14:04 +0100

    Split `XRay.Particles.API` out of `XRay.Particles`

---
+++ src/xrParticles/cmake/XRay.Particles.API.cmake
@@ -0,0 +1,41 @@
+add_library(XRay.Particles.API STATIC)
+
+set_property(
+  TARGET XRay.Particles.API
+  PROPERTY FOLDER
+  ${FOLDER_XRAY_PARTICLES}
+)
+
+target_sources(XRay.Particles.API
+  PRIVATE
+  noise.cpp
+  particle_actions.cpp
+  particle_actions_collection.cpp
+  particle_actions_collection_io.cpp
+  particle_core.cpp
+  particle_effect.cpp
+  particle_manager.cpp
+  
+  noise.h
+  particle_actions.h
+  particle_actions_collection.h
+  particle_core.h
+  particle_effect.h
+  particle_manager.h
+)
+
+target_include_directories(XRay.Particles.API
+  PRIVATE
+  ${CMAKE_CURRENT_SOURCE_DIR}
+)
+
+target_precompile_headers(XRay.Particles.API
+  PRIVATE stdafx.h
+)
+
+target_link_libraries(XRay.Particles.API
+  PRIVATE
+  tbb
+  XRay.Core
+  XRay.CPUPipe
+)
