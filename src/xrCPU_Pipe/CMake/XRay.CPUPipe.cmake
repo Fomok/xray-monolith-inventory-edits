924d2bf3547df019fc6c2607fab3ee1538650989
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-03 04:36:14 +0100

    Split `XRay.CPUPipe` into submodules

--- src/xrCPU_Pipe/CMake/XRay.CPUPipe.cmake
+++ src/xrCPU_Pipe/CMake/XRay.CPUPipe.cmake
@@ -10,20 +10,37 @@ add_module(XRay.CPUPipe
 
   SOURCES
   xrCPU_Pipe.cpp
-  
-  StdAfx.h
   xrCPU_Pipe.h
-  
+)
+
+add_module(XRay.CPUPipe.PLC
+  PARENT XRay.CPUPipe
+
+  SOURCES
   PLC.cpp
-  
+)
+
+add_module(XRay.CPUPipe.Resources
+  PARENT XRay.CPUPipe
+
+  SOURCES
   resource.h
   xrCPU_Pipe.rc
-  
+)
+
+add_module(XRay.CPUPipe.Skinning
+  PARENT XRay.CPUPipe
+
+  SOURCES
   xrSkin2W.cpp
   xrSkin2W_SSE.cpp
   xrSkin2W_thread.cpp
-  
+)
+
+add_module(XRay.CPUPipe.TTAPI
+  PARENT XRay.CPUPipe
+
+  SOURCES
   ttapi.cpp
-  
   ttapi.h
 )
