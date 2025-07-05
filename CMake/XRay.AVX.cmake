36b89e95d635fa272957ea31b4e8959a3e76f154
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-05 03:51:19 +0100

    Clang awareness for `XRay.AVX`

--- CMake/XRay.AVX.cmake
+++ CMake/XRay.AVX.cmake
@@ -1,5 +1,9 @@
 if(${XRAY_AVX})
-  add_compile_options(/arch:AVX)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL MSVC)
+    add_compile_options(/arch:AVX)
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL Clang)
+    add_compile_options(-mavx)
+  endif()
 endif()
 
 message(STATUS "          AVX: ${XRAY_AVX}")
