39c292de80b7335a14c91872a9fcc20d1019567e
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-05 06:23:11 +0100

    `graph_edge_inline.h`: Fully quality `CSGraphEdge::_vertex_id_type`

--- src/xrGame/graph_edge_inline.h
+++ src/xrGame/graph_edge_inline.h
@@ -24,7 +24,7 @@ IC CSGraphEdge::CEdgeBase(const _edge_weight_type& weight, _vertex_type* vertex)
 }
 
 TEMPLATE_SPECIALIZATION
-IC typename const CSGraphEdge::_edge_weight_type&CSGraphEdge::weight() const
+IC typename const CSGraphEdge::_edge_weight_type& CSGraphEdge::weight() const
 {
 	return (m_weight);
 }
@@ -36,7 +36,7 @@ IC typename CSGraphEdge::_vertex_type*CSGraphEdge::vertex() const
 }
 
 TEMPLATE_SPECIALIZATION
-IC const typename CSGraphEdge::_vertex_id_type&CSGraphEdge::vertex_id() const
+IC const typename CSGraphEdge::_vertex_id_type& CSGraphEdge::vertex_id() const
 {
 	return (vertex()->vertex_id());
 }
@@ -63,7 +63,7 @@ IC CSGraphEdge::CEdge(const _edge_weight_type& weight, _vertex_type* vertex) :
 }
 
 TEMPLATE_SPECIALIZATION
-IC bool CSGraphEdge::operator==(const _vertex_id_type& vertex_id) const
+IC bool CSGraphEdge::operator==(const CSGraphEdge::_vertex_id_type& vertex_id) const
 {
 	return (vertex()->vertex_id() == vertex_id);
 }
@@ -110,7 +110,7 @@ IC CSGraphEdge::CEdge(const _edge_weight_type& weight, _vertex_type* vertex) :
 }
 
 TEMPLATE_SPECIALIZATION
-IC bool CSGraphEdge::operator==(const _vertex_id_type& vertex_id) const
+IC bool CSGraphEdge::operator==(const CSGraphEdge::_vertex_id_type& vertex_id) const
 {
 	return (vertex()->vertex_id() == vertex_id);
 }
