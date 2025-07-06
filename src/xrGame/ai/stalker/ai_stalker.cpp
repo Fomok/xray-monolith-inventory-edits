b0281bb8521866c0bc02f821b2e93227cc9a89fc
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-06 19:12:14 +0100

    `ai_stalker.cpp`: Explicitly cast to `DLL_Pure*` before return

--- src/xrGame/ai/stalker/ai_stalker.cpp
+++ src/xrGame/ai/stalker/ai_stalker.cpp
@@ -115,7 +115,7 @@ void CAI_Stalker::reinit()
 	animation().reinit();
 	//	movement().reinit				();
 
-	//загрузка спецевической звуковой схемы для сталкера согласно m_SpecificCharacter
+	//Г§Г ГЈГ°ГіГ§ГЄГ  Г±ГЇГҐГ¶ГҐГўГЁГ·ГҐГ±ГЄГ®Г© Г§ГўГіГЄГ®ГўГ®Г© Г±ГµГҐГ¬Г» Г¤Г«Гї Г±ГІГ Г«ГЄГҐГ°Г  Г±Г®ГЈГ«Г Г±Г­Г® m_SpecificCharacter
 	sound().sound_prefix(SpecificCharacter().sound_voice_prefix());
 
 #ifdef DEBUG_MEMORY_MANAGER
@@ -601,7 +601,7 @@ void CAI_Stalker::Die(CObject* who)
 
 	inherited::Die(who);
 
-	//запретить использование слотов в инвенторе
+	//Г§Г ГЇГ°ГҐГІГЁГІГј ГЁГ±ГЇГ®Г«ГјГ§Г®ГўГ Г­ГЁГҐ Г±Г«Г®ГІГ®Гў Гў ГЁГ­ГўГҐГ­ГІГ®Г°ГҐ
 	inventory().SetSlotsUseful(false);
 
 	if (inventory().GetActiveSlot() == NO_ACTIVE_SLOT)
@@ -715,7 +715,7 @@ BOOL CAI_Stalker::net_Spawn(CSE_Abstract* DC)
 	if (!g_Alive())
 		sound().set_sound_mask(u32(eStalkerSoundMaskDie));
 
-	//загрузить иммунитеты из модельки сталкера
+	//Г§Г ГЈГ°ГіГ§ГЁГІГј ГЁГ¬Г¬ГіГ­ГЁГІГҐГІГ» ГЁГ§ Г¬Г®Г¤ГҐГ«ГјГЄГЁ Г±ГІГ Г«ГЄГҐГ°Г 
 	IKinematics* pKinematics = smart_cast<IKinematics*>(Visual());
 	VERIFY(pKinematics);
 	CInifile* ini = pKinematics->LL_UserData();
@@ -734,7 +734,7 @@ BOOL CAI_Stalker::net_Spawn(CSE_Abstract* DC)
 		}
 	}
 
-	//вычислить иммунета в зависимости от ранга
+	//ГўГ»Г·ГЁГ±Г«ГЁГІГј ГЁГ¬Г¬ГіГ­ГҐГІГ  Гў Г§Г ГўГЁГ±ГЁГ¬Г®Г±ГІГЁ Г®ГІ Г°Г Г­ГЈГ 
 	static float novice_rank_immunity = pSettings->r_float("ranks_properties", "immunities_novice_k");
 	static float expirienced_rank_immunity = pSettings->r_float("ranks_properties", "immunities_experienced_k");
 
@@ -1375,7 +1375,7 @@ DLL_Pure* CAI_Stalker::_construct()
 		Msg								("CAI_Stalker::_construct() : %lld",Memory.mem_usage() - start);
 #endif // DEBUG_MEMORY_MANAGER
 
-	return (this);
+	return (DLL_Pure*)(this);
 }
 
 bool CAI_Stalker::use_center_to_aim() const
