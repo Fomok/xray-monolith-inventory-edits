8d4a40aa8b70cdfa0bfb1cbc6a3ee795fa132ba0
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-06 19:11:12 +0100

    `ResourceManager.cpp`: Use `_strlwr`

--- src/Layers/xrRender/ResourceManager.cpp
+++ src/Layers/xrRender/ResourceManager.cpp
@@ -116,7 +116,7 @@ void CResourceManager::_ParseList(sh_list& dest, LPCSTR names)
 		{
 			// flush
 			N.push_back(0);
-			strlwr(N.begin());
+			_strlwr(N.begin());
 
 			fix_texture_name(N.begin());
 			//. andy			if (strext(N.begin())) *strext(N.begin())=0;
@@ -133,7 +133,7 @@ void CResourceManager::_ParseList(sh_list& dest, LPCSTR names)
 	{
 		// flush
 		N.push_back(0);
-		strlwr(N.begin());
+		_strlwr(N.begin());
 
 		fix_texture_name(N.begin());
 		//. andy		if (strext(N.begin())) *strext(N.begin())=0;
