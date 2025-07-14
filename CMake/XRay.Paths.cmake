9d70790c42b5dffb2d0e89ffe8109638f7099567
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-15 00:50:39 +0100

    Update SDK paths

--- CMake/XRay.Paths.cmake
+++ CMake/XRay.Paths.cmake
@@ -1,7 +1,7 @@
 include_guard()
 
 # Define SDK paths
-set(XRAY_SDK ${CMAKE_SOURCE_DIR}/sdk)
+set(XRAY_SDK ${CMAKE_SOURCE_DIR}/Extern/SDK)
 set(XRAY_SDK_BIN ${XRAY_SDK}/bin)
 set(XRAY_SDK_INCLUDE ${XRAY_SDK}/include)
 set(XRAY_SDK_LIB ${XRAY_SDK}/libraries/x64)
