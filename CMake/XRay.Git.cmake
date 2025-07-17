ff0ece3ddfcf4519dee3c2854e94e8bbd83bbeee
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-17 03:49:07 +0100

    Don't init submodules recursively

--- CMake/XRay.Git.cmake
+++ CMake/XRay.Git.cmake
@@ -5,11 +5,11 @@ if(GIT_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
     if(GIT_SUBMODULE)
         message(STATUS)
         message(STATUS "Updating git submodules...")
-        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive
+        execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init
                         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                         RESULT_VARIABLE GIT_SUBMOD_RESULT)
         if(NOT GIT_SUBMOD_RESULT EQUAL "0")
-            message(FATAL_ERROR "git submodule update --init --recursive failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+            message(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
         endif()
         message(STATUS "Done")
     endif()
