c0f9ad008832d3d6516390830fdf1576a2586416
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-17 02:28:37 +0100

    Transition `libtheora` to a submodule library

--- CMake/XRay.Sources.cmake
+++ CMake/XRay.Sources.cmake
@@ -22,10 +22,10 @@ function(target_sources_sandboxed TARGET)
       set(DEST "${ARG_DIRECTORY}/${DEST}")
     endif()
 
-    message("Copying ${SOURCE} to ${DEST}")
+    message(DEBUG "Copying ${SOURCE} to ${DEST}")
     configure_file(${SOURCE} ${DEST} COPYONLY)
 
-    message("Adding target source: ${CMAKE_CURRENT_BINARY_DIR}/${DEST}")
+    message(DEBUG "Adding target source: ${CMAKE_CURRENT_BINARY_DIR}/${DEST}")
     target_sources(${TARGET} PRIVATE "${CMAKE_CURRENT_BINARY_DIR}/${DEST}")
   endforeach()
 endfunction()
