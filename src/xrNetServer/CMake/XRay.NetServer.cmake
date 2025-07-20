c735bd882f0c42c4aaa45e354b385df82b6b9be1
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-07-20 23:41:27 +0100

    Convert `XRay.NetServer` to full CMake PCH

--- src/xrNetServer/CMake/XRay.NetServer.cmake
+++ src/xrNetServer/CMake/XRay.NetServer.cmake
@@ -1,9 +1,14 @@
 add_module(XRay.NetServer
   TYPE STATIC
   
-  INCLUDES ${CMAKE_CURRENT_SOURCE_DIR}
+  INCLUDES
+  ${CMAKE_CURRENT_SOURCE_DIR}
 
-  PRECOMPILES stdafx.h
+  PRECOMPILES
+  [["xrCore.h"]]
+  [["DPlay/dplay8.h"]]
+  NET_Shared.h
+  macros.h
 
   LINKS
   DPlay
@@ -13,25 +18,32 @@ add_module(XRay.NetServer
   XRay.Core
 
   SOURCES
-  ip_filter.cpp
-  NET_AuthCheck.cpp
-  NET_Client.cpp
-  NET_Common.cpp
-  NET_Compressor.cpp
-  NET_Log.cpp
-  NET_Server.cpp
+  macros.h
 
+  ip_filter.cpp
   ip_filter.h
+  
+  NET_AuthCheck.cpp
   NET_AuthCheck.h
+
+  NET_Client.cpp
   NET_Client.h
+
+  NET_Common.cpp
   NET_Common.h
+
+  NET_Compressor.cpp
   NET_Compressor.h
+
+  NET_Log.cpp
   NET_Log.h
+
+  NET_Server.cpp
+  NET_Server.h
+
   NET_Messages.h
   NET_PlayersMonitor.h
-  NET_Server.h
   NET_Shared.h
-  stdafx.h
 )
 
 set_source_files_properties(
