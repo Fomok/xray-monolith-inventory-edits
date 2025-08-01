2a6261982fff261f5ae21e9871de0ab58e421264
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-08-01 03:23:53 +0100

    Factor inert includes out of `xrRenderPC_R3/stdafx.h`

--- src/Layers/xrRenderPC_R3/stdafx.h
+++ src/Layers/xrRenderPC_R3/stdafx.h
@@ -4,23 +4,13 @@
 
 #pragma once
 
-#include "../xrRender/xrD3DDefs.h"
-
 #include "../xrRender/Debug/dxPixEventWrapper.h"
 
-#include "../../xrParticles/psystem.h"
-
-#include "../xrRender/HW.h"
-#include "../xrRender/Shader.h"
 #include "../xrRender/R_Backend.h"
 #include "../xrRender/R_Backend_Runtime.h"
 
 #include "../xrRender/resourcemanager.h"
 
-#include "../../xrEngine/vis_common.h"
-#include "../../xrEngine/render.h"
-#include "../../xrEngine/_d3d_extensions.h"
-#include "../../xrEngine/igame_level.h"
 #include "../xrRender/blenders\blender.h"
 #include "../xrRender/blenders\blender_clsid.h"
 #include "../xrRender/xrRender_console.h"
