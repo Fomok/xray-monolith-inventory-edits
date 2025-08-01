86a3b4a2ff5a5dd189df16842c434a8d8ac47ae9
ProfLander <1253239+ProfLander@users.noreply.github.com.>
2025-08-01 04:39:22 +0100

    Factor inert imports out of `xrRenderPC_R4/stdafx.h`

--- src/Layers/xrRenderPC_R4/stdafx.h
+++ src/Layers/xrRenderPC_R4/stdafx.h
@@ -6,19 +6,12 @@
 
 #include "../xrRenderDX10/dx10EventWrapper.h"
 
-#include "../../xrParticles/psystem.h"
-
-#include "../xrRender/HW.h"
 #include "../xrRender/Shader.h"
 #include "../xrRender/R_Backend.h"
 #include "../xrRender/R_Backend_Runtime.h"
 
 #include "../xrRender/resourcemanager.h"
 
-#include "../../xrEngine/vis_common.h"
-#include "../../xrEngine/render.h"
-#include "../../xrEngine/_d3d_extensions.h"
-#include "../../xrEngine/igame_level.h"
 #include "../xrRender/blenders\blender.h"
 #include "../xrRender/blenders\blender_clsid.h"
 #include "../xrRender/xrRender_console.h"
